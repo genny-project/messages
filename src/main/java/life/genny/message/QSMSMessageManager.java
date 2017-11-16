6c134c9a664cad6a5d8778e893fb97d9d38a37f0
rpgayatri <rpgayatri@gmail.com>
2017-11-16 16:41:10 +1100

    editing smsmanager file

--- src/main/java/life/genny/message/QSMSMessageManager.java
+++ src/main/java/life/genny/message/QSMSMessageManager.java
@@ -18,24 +18,4 @@ public class QSMSMessageManager implements QMessageProvider {
 		
 	}
 
-	/*
-	 * public static final String ACCOUNT_SID =
-	 * "AC6bd0bd2ecad9cc99d01a14942e0095a4"; public static final String
-	 * AUTH_TOKEN = "f5d7bc286e850b927876fa288a0b7be1";
-	 * 
-	 * public static void sendSMS(String message, String fromNumber, String
-	 * toNumber) {
-	 * 
-	 * Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
-	 * 
-	 * //PhoneNumber numberTo = PhoneNumber.fetcher(new
-	 * com.twilio.type.PhoneNumber("+61468497227")).fetch();
-	 * 
-	 * Message msg = Message.creator(new PhoneNumber(toNumber), new
-	 * PhoneNumber(fromNumber), message).create();
-	 * 
-	 * System.out.println("message status:"+msg.getStatus()+", message SID:"+msg
-	 * .getSid()); }
-	 */
-
 }
