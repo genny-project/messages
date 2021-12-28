71c2130891bc56b385e3e82c3672eacd94d7bbec
mnzit <mnzitshakya@gmail.com>
2021-12-28 08:09:55 +0545

    StringBuilder for encoding url

--- src/main/java/life/genny/messages/util/MsgUtils.java
+++ src/main/java/life/genny/messages/util/MsgUtils.java
@@ -1,5 +1,7 @@
 package life.genny.messages.util;
 
+import org.apache.http.util.Asserts;
+
 import java.util.Base64;
 
 public class MsgUtils {
@@ -42,4 +44,42 @@ public class MsgUtils {
 		return url;
 	}
 
+	/**
+	 * Uses StringBuilder Pattern
+	 * @param base
+	 * @param parentCode
+	 * @param code
+	 * @param targetCode
+	 * @param token
+	 * @return
+	 */
+	public static String encodedUrlBuilder(String base, String parentCode, String code, String targetCode, String token) {
+		Asserts.notNull(base, "Base is null");
+		/**
+		 * A Function for Base64 encoding urls
+		 **/
+		StringBuilder url = new StringBuilder();
+		// Encode Parent and Code
+		url
+				.append(base)
+				.append("/")
+				.append(Base64.getEncoder().encodeToString(parentCode.getBytes()))
+				.append("/")
+				.append(Base64.getEncoder().encodeToString(code.getBytes()));
+
+		// Add encoded targetCode if not null
+		if (targetCode != null) {
+			url
+					.append("/")
+					.append(Base64.getEncoder().encodeToString(targetCode.getBytes()));
+		}
+
+		// Add access token if not null
+		if (token != null) {
+			url
+					.append("?token=")
+					.append(token);
+		}
+		return url.toString();
+	}
 }
