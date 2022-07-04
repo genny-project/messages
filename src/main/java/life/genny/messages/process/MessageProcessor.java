adc4881e0569df57d21bc5378d4db4ccfb42b657
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2022-07-04 12:42:58 +1000

    String isEmpty check added

--- src/main/java/life/genny/messages/process/MessageProcessor.java
+++ src/main/java/life/genny/messages/process/MessageProcessor.java
@@ -98,8 +98,11 @@ public class MessageProcessor {
             // Handle any default context associations
             String contextAssociations = templateBe.getValue("PRI_CONTEXT_ASSOCIATIONS", null);
             log.info("PRI_CONTEXT_ASSOCIATIONS is -> " + contextAssociations);
-            if (contextAssociations != null) {
+
+            if (contextAssociations != null || !contextAssociations.isEmpty()) {
                 MergeUtils.addAssociatedContexts(beUtils, baseEntityContextMap, contextAssociations, false);
+            } else {
+                log.info("PRI_CONTEXT_ASSOCIATIONS is empty!");
             }
 
             // Check for Default Message
