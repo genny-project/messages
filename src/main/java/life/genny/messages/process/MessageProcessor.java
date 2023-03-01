c957b64428f668668c49a086227db285c10d6eea
mnzit <mnzitshakya@gmail.com>
2023-03-01 12:54:29 +0545

    changes

--- src/main/java/life/genny/messages/process/MessageProcessor.java
+++ src/main/java/life/genny/messages/process/MessageProcessor.java
@@ -113,7 +113,7 @@ public class MessageProcessor {
             if (Arrays.stream(message.getMessageTypeArr()).anyMatch(item -> item == QBaseMSGMessageType.DEFAULT)) {
                 // Use default if told to do so
                 List<String> typeList = beUtils.getBaseEntityCodeArrayFromLNKAttr(templateBe, "PRI_DEFAULT_MSG_TYPE");
-                Object body  = beUtils.getBaseEntityValue(templateBe, "PRI_BODY");
+                String body  = templateBe.getValue("PRI_BODY", null);
                 System.out.println("### body: "+ body);
                 System.out.println("### typeList: "+ Arrays.asList(typeList));
                 try {
